c6cd2aa2dd898210d7b3b12800943f58665330c4
anastasiya <nastya.panysh@gmail.com>
2022-10-02 18:16:21 +0300

    edit DOM tasks

--- DOM/5/app.js
+++ DOM/5/app.js
@@ -1,15 +1,22 @@
 // Необходимо отобразить числовой инпут и кнопку. После нажатия на кнопку необходимо получить значение из инпута. Проверить, что это число > 0. Если проверка true, то вывести в Результат ряд Фибоначчи. Рекурсия
 
 const btn = document.querySelector('.btn')
-let input = document.querySelector('.input');
+const input = +document.querySelector('.input');
 
 
 function fibonacci(input) {
-    return input.value <= 1 ? input.value : fibonacci(input.value - 1) + fibonacci(input.value - 2)
+    if (input.value <= 1) return input.value
+    return fibonacci(input.value - 1) + fibonacci(input.value - 2)
 }
 
 btn.addEventListener('click', () => {
-    const arr = fibonacci(input)
+    // typeof input.value ==='number'
+    const arr = [];
+
+    for (let i = 0; i < input.value; i++) {
+        arr.push(fibonacci(input))
+    }
+    console.log(arr);
     document.querySelector('.res').innerHTML = `результат: ${arr}`;
 })
 
