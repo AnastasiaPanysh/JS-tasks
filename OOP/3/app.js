eefa9579f8c5fe5b6883815f275a4fc92211854f
anastasiya <nastya.panysh@gmail.com>
2022-10-14 22:29:28 +0300

    add tasks 4

--- OOP/3/app.js
+++ OOP/3/app.js
@@ -2,3 +2,26 @@
 // reverseString, переворачивающий строку, метод upperFirst, возвращающий строку,
 // где первая буква заглавная и метод upperEvery, который делает заглавной
 // первую букву каждого слова этой строки.
+
+class WordString {
+
+    reverseString(str) {
+        return str.split('').reverse().join('')
+
+    }
+
+    upperFirst(str) {
+        return str[0].toUpperCase() + str.slice(1)
+    }
+
+    upperEvery(str) {
+        return str.toUpperCase()
+    }
+
+
+}
+
+const wordString = new WordString()
+console.log(wordString.reverseString('hschool'));
+console.log(wordString.upperFirst('hschool'));
+console.log(wordString.upperEvery('hschool'));
--- OOP/4/app.js
+++ OOP/4/app.js
@@ -2,3 +2,17 @@
 // элементов массива. На основании числа формируется динамический массив из n
 // элементов внутри класса. Создать функцию для подсчета всех четных чисел
 // массива. Добавить проверки на ввод
+
+class MathСalculation {
+   doArr(n) {
+
+   }
+   
+
+   checkArr(arr) {
+
+   }
+}
+
+const mathСalculation = new MathСalculation();
+console.log(mathСalculation.checkArr);
