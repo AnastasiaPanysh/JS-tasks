277c978eb3be7b10a79f2e6476206b343bd0930a
anastasiya <nastya.panysh@gmail.com>
2022-10-15 22:57:55 +0300

    add 9-12

--- OOP/12/app.js
+++ OOP/12/app.js
@@ -2,3 +2,26 @@
 // массива. На основании числа формируется динамический массив из n элементов
 // внутри класса. Создать функцию doHashtag, заполняющую новый массив из
 // элементов, каждое значение которого имеет вид #name
+
+class Hashtag {
+
+    doArr(n) {
+        let arr = []
+        for (let i = 0; i < n; i++) {
+            arr.push(Math.floor(Math.random() * 100))
+        }
+        const newArr = this.doHashtag(arr);
+        return newArr
+    }
+
+    doHashtag(newArr) {
+        let hashtagArr = []
+        for (let i = 0; i < newArr.length; i++) {
+            hashtagArr.push('#' + newArr[i])
+        }
+        return hashtagArr
+    }
+}
+
+const hashtag = new Hashtag();
+console.log(hashtag.doArr(3));
