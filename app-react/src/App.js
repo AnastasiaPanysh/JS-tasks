c0ed8a3c3ad4afc1f43ebb03cdaa773a25763a13
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-03-06 16:54:54 +0300

    add  task 17 react

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,26 +1,26 @@
 
-import Task1 from './pages/Task1';
-import Task2 from './pages/Task2';
-import Task3 from './pages/Task3';
-import Task4 from './pages/Task4';
-import Task5 from './pages/Task5';
-import Task6 from './pages/Task6';
-import Task7 from './pages/Task7';
-import Task8 from './pages/Task8';
-import Task9 from './pages/Task9';
-import Task10 from './pages/Task10';
-import Task11 from './pages/Task11';
-import Task12 from './pages/Task12';
-import Task13 from './pages/Task13';
-import Task14 from './pages/Task14';
-import Task15 from './pages/Task15';
-import Task16 from './pages/Task16';
+// import Task1 from './pages/Task1';
+// import Task2 from './pages/Task2';
+// import Task3 from './pages/Task3';
+// import Task4 from './pages/Task4';
+// import Task5 from './pages/Task5';
+// import Task6 from './pages/Task6';
+// import Task7 from './pages/Task7';
+// import Task8 from './pages/Task8';
+// import Task9 from './pages/Task9';
+// import Task10 from './pages/Task10';
+// import Task11 from './pages/Task11';
+// import Task12 from './pages/Task12';
+// import Task13 from './pages/Task13';
+// import Task14 from './pages/Task14';
+// import Task15 from './pages/Task15';
+// import Task16 from './pages/Task16';
 import Task17 from './pages/Task17';
 
 function App() {
   return (
     <>
-   <Task1></Task1>
+   {/* <Task1></Task1>
    <hr></hr>
    <Task2></Task2>
    <hr></hr>
@@ -51,7 +51,7 @@ function App() {
    <Task15></Task15>
    <hr></hr>
    <Task16></Task16>
-   <hr></hr>
+   <hr></hr> */}
    <Task17></Task17>
    </>
   );
--- app-react/src/pages/Task17.js
+++ app-react/src/pages/Task17.js
@@ -2,11 +2,29 @@ import { useState } from 'react'
 
 
 function Task17() {
+    const [value, setValue] = useState(showContent('About'))
 
+    function getValue(event) {
+        setValue(showContent(event.target.textContent))
+    }
+
+    function showContent(content) {
+        if (content === 'About') return <p>{content}</p>
+        else if (content === 'Blog') return <p>{content}</p>
+        else if (content === 'Programm') return <p>{content}</p>
+    }
 
     return (
         <>
+            <div onClick={getValue}>
+                <p>About</p>
+                <p>Blog</p>
+                <p>Programm</p>
+            </div>
 
+            <div>
+                {value}
+            </div>
         </>
     )
 }
